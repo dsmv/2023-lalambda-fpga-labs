3669e4e88ebfe4183d1566295bd1a94a0adbe06c
dsmv <dsmv2011@gmail.com>
2023-07-08 11:45:16 +0400

    delete

--- lab_ext_svga_base/src/verilog.f
+++
@@ -1,9 +0,0 @@
--i src/school_risk
-src/school_risk/sm_register.v
-src/school_risk/sm_register_we.v
-src/school_risk/sm_register_file_vc.v
-src/school_risk/sm_rom.v
-src/school_risk/sr_decode.v
-src/school_risk/sr_alu.v
-src/school_risk/sr_control.v
-src/school_risk/sr_cpu_vc.v
